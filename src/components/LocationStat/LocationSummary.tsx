aa214e88b707ad08a84a106f1faac4295d9f0a53
Mt. Front <mfcndw@gmail.com>
2025-05-03 12:23:43 -0700

    change words

--- src/components/LocationStat/LocationSummary.tsx
+++ src/components/LocationStat/LocationSummary.tsx
@@ -7,8 +7,8 @@ const LocationSummary = () => {
   return (
     <div className="cursor-pointer">
       <section>
-        {years ? <Stat value={`${years.length}`} description=" 年里我走过" /> : null}
-        {countries ? <Stat value={countries.length} description=" 个国家" /> : null}
+        {countries ? <Stat value={countries.length} description=" countries" /> : null}
+        {years ? <Stat value={`${years.length}`} description=" years logged" /> : null}
         {/* {provinces ? <Stat value={provinces.length} description=" 个省份" /> : null}
         {cities ? (
           <Stat value={Object.keys(cities).length} description=" 个城市" />
